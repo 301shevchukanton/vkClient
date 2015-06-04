fa1ed70b47583c9698f818ee383e4e841eabebeb
Podkaifom <301.shevchuk.anton@gmail.com>
2015-06-04 21:57:44 +0300

    Cleaned code

---
+++ VkClient/VkClient/app/src/main/java/com/vkclient/Adapters/DialogsListViewAdapter.java
@@ -0,0 +1,53 @@
+package com.vkclient.Adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.vkclient.Classes.Dialog;
+import com.example.podkaifom.vkclient.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by pod kaifom on 02.06.2015.
+ */
+public class DialogsListViewAdapter extends ArrayAdapter<Dialog> {
+    private List<Dialog> mModels;
+    public Object getItems() {
+        return mModels;
+    }
+    public DialogsListViewAdapter(Context context, List<Dialog> models)
+    {
+        super(context, R.layout.dialogs_list_item, R.id.dialog_name, models);
+        mModels = models;
+    }
+    @Override
+    public View getView(int position, final View convertView, ViewGroup parent) {
+        final View view = super.getView(position, convertView, parent);
+        final Dialog dialog = getItem(position);
+        ((TextView) view.findViewById(R.id.dialog_name)).setText(dialog.getUsername());
+        ((TextView) view.findViewById(R.id.dialog_date)).setText(dialog.getParsedDate().toString("dd.MM - HH:mm"));
+        ((ImageView) view.findViewById(R.id.dialog_photo)).setImageResource(R.drawable.user_100);
+        ((TextView) view.findViewById(R.id.dialog_text)).setText(dialog.getBody());
+        if (dialog.getReadState() == false)
+             view.findViewById(R.id.dialog_text).setBackgroundColor(Color.LTGRAY);
+        else
+            view.findViewById(R.id.dialog_text).setBackgroundColor(Color.TRANSPARENT);
+
+      try {
+          if(!dialog.getGetphoto().isEmpty())       Picasso.with(getContext())
+                                                    .load(dialog.getGetphoto())
+                                                    .into((ImageView)view.findViewById(R.id.dialog_photo));
+        }
+      catch (Exception e){}
+        return view;
+
+    }
+
+}
---
+++ VkClient/VkClient/app/src/main/java/com/vkclient/Classes/Dialog.java
@@ -0,0 +1,84 @@
+package com.vkclient.Classes;
+
+import org.joda.time.DateTime;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.TimeZone;
+
+/**
+ * Created by pod kaifom on 02.06.2015.
+ */
+public class Dialog {
+    private final int id;
+    private final long date;
+    private final int user_id;
+    private final boolean readState;
+    private final String title;
+    private final String body;
+    private String photo_200;
+    private  String username="";
+public Dialog(int id, long date, int user_id, boolean readState, String title, String body){
+    this.id = id;
+    this.date=date;
+    this.user_id=user_id;
+    this.readState=readState;
+    this.title=title;
+    this.body=body;
+}
+    public int getId() {
+        return id;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public boolean getReadState() {
+        return readState;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+    public  DateTime getParsedDate()
+    {
+        DateTime dateTime = new DateTime( ( (long)this.date * 1000L + TimeZone.getDefault().getRawOffset()));
+
+        return dateTime;
+    }
+    public void setUsername(String name){
+        this.username=name;
+    }
+    public String getUsername(){
+       return this.username;
+    }
+    public void setPhoto(String photo){
+        this.photo_200=photo;
+    }
+    public String getGetphoto(){
+        return this.photo_200;
+    }
+    public static Dialog parseDialog(JSONObject object){
+        Dialog result=null;
+        try{
+                result = new Dialog(Integer.parseInt(object.getString("id")),
+                        Long.parseLong(object.getString("date")),
+                        Integer.parseInt(object.getString("user_id")),
+                        (object.getString("read_state").equals("1")),
+                        object.getString("title"),
+                        object.getString("body"));
+            } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
