d93f045b6b9d7ac5673618e5867e12d0821e7dd7
Podkaifom <301.shevchuk.anton@gmail.com>
2015-07-28 01:32:07 +0300

    cleaned code, refactoring

--- VkClient/app/src/main/java/com/vkclient/views/FriendsListItemView.java
+++ VkClient/app/src/main/java/com/vkclient/views/FriendsListItemView.java
@@ -13,7 +13,6 @@ import com.vkclient.adapters.FriendListAdapter;
 import com.vkclient.entities.User;
 import com.vkclient.supports.PhotoLoader;
 
-import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 
 public class FriendsListItemView extends LinearLayout {
@@ -21,6 +20,7 @@ public class FriendsListItemView extends LinearLayout {
     private FriendListAdapter.OnPhotoClickListener photoClickListener;
     private TextView friendName;
     private TextView friendBirthDate;
+    private User currentFriend;
 
     public FriendsListItemView(Context context) {
         this(context, null);
@@ -48,24 +48,22 @@ public class FriendsListItemView extends LinearLayout {
 
     public void setFriend(final User friend) {
         this.friendName.setText(friend.getName());
-        this.friendPhoto.setOnClickListener(
-                new ImageView.OnClickListener() {
-                    public void onClick(View v) {
-                        if (FriendsListItemView.this.photoClickListener != null) {
-                            photoClickListener.onClick(String.valueOf(friend.getId()));
-                        }
-                    }
-                }
-        );
+        this.currentFriend = friend;
+        this.friendPhoto.setOnClickListener(photoViewListener);
         friendPhoto.setImageResource(R.drawable.ic_user100);
         PhotoLoader.loadPhoto(getContext(), friend.getPhoto(), friendPhoto);
-        String birthDateStr = String.valueOf(R.string.not_set);
-        DateTime dt = friend.getBirthDate();
-        if (dt != null) {
-            birthDateStr = dt.toString(DateTimeFormat.forPattern(friend.getDateFormat()));
-        }
-        this.friendBirthDate.setText(birthDateStr);
+        String birthDateString;
+        birthDateString = friend.getBirthDate() != null ? friend.getBirthDate().toString(DateTimeFormat.forPattern(friend.getDateFormat())) :
+                String.valueOf(R.string.not_set);
+        this.friendBirthDate.setText(birthDateString);
     }
 
+    private ImageView.OnClickListener photoViewListener = new ImageView.OnClickListener() {
+        public void onClick(View v) {
+            if (FriendsListItemView.this.photoClickListener != null) {
+                photoClickListener.onClick(String.valueOf(currentFriend.getId()));
+            }
+        }
+    };
 
 }
